a6e7982be6f13d16a51d94ddd23fa7557ad1b052
trunglt251292 <trunglt251292@gmail.com>
2022-03-19 12:08:07 +0700

    [Update] Remove queue

--- constants.go
+++ constants.go
@@ -2,17 +2,14 @@ package elasticsearch
 
 const (
 	SubjectRequestProductUpsert = "elasticsearch/selly.request.product.upsert"
-	SubjectQueueProductUpsert   = "elasticsearch/selly.queue.product.upsert"
 	SubjectPullProductUpsert    = "elasticsearch/selly.pull.product.upsert"
 	SubjectRequestProductSearch = "elasticsearch/selly.request.product.search"
 
 	SubjectRequestOrderUpsert = "elasticsearch/selly.request.order.upsert"
-	SubjectQueueOrderUpsert   = "elasticsearch/selly.queue.order.upsert"
 	SubjectPullOrderUpsert    = "elasticsearch/selly.pull.order.upsert"
 	SubjectRequestOrderSearch = "elasticsearch/selly.request.order.search"
 
 	SubjectRequestUserUpsert = "elasticsearch/selly.request.order.upsert"
-	SubjectQueueUserUpsert   = "elasticsearch/selly.queue.order.upsert"
 	SubjectPullUserUpsert    = "elasticsearch/selly.pull.order.upsert"
 	SubjectRequestUserSearch = "elasticsearch/selly.request.user.search"
 )
